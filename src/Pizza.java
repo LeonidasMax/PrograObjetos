2a4d08243ff51972ee62879ced479e2e8d2d42dc
LeonidasMax <140268906+LeonidasMax@users.noreply.github.com>
2023-08-18 20:08:22 -0600

    Update Pizza.java

--- src/Pizza.java
+++ src/Pizza.java
@@ -8,27 +8,18 @@ public class Pizza {
         String nombre;
         double precio;
         List<String> Lis_ingredientes = new ArrayList<>();
-
-        Lis_ingredientes.add("Masa de pizza");
-        Lis_ingredientes.add("Salsa de tomate");
-        Lis_ingredientes.add("Queso rallado");
-        Lis_ingredientes.add("Aceitunas negras");
-        Lis_ingredientes.add("Cebolla picada");
-        Lis_ingredientes.add("Salchicha");
-        Lis_ingredientes.add("Champiñones");
-        Lis_ingredientes.add("Pimiento");
-
-// Print the list
+            Lis_ingredientes.add("Masa de pizza");
+            Lis_ingredientes.add("Salsa de tomate");
+            Lis_ingredientes.add("Queso rallado");
+            Lis_ingredientes.add("Aceitunas negras");
+            Lis_ingredientes.add("Cebolla picada");
+            Lis_ingredientes.add("Salchicha");
+            Lis_ingredientes.add("Champiñones");
+            Lis_ingredientes.add("Pimiento");
+/* La lista de Strings no pueden ser constantes debido a que se pueden modificar dinámicamente solo a
+variables tales como "final double pi=3.1415926536;"
+ */
         System.out.println(Lis_ingredientes);
 
-    }
-}
-
-
-
-
-
-
-
-
+    }}
 
