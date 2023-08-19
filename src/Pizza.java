b87a521b275f35666da590f4dd49557e71ea4c7e
Leonidas <ovalladareso@miumg.edu.gt>
2023-08-18 19:28:55 -0600

    Initial commit

---
+++ src/Pizza.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pizza {
+    public static void main(String[] args) {
+
+
+        String nombre;
+        double precio;
+        List<String> Lis_ingredientes = new ArrayList<>();
+
+        Lis_ingredientes.add("Masa de pizza");
+        Lis_ingredientes.add("Salsa de tomate");
+        Lis_ingredientes.add("Queso rallado");
+        Lis_ingredientes.add("Aceitunas negras");
+        Lis_ingredientes.add("Cebolla picada");
+        Lis_ingredientes.add("Salchicha");
+        Lis_ingredientes.add("Champiñones");
+        Lis_ingredientes.add("Pimiento");
+
+// Print the list
+        System.out.println(Lis_ingredientes);
+
+    }
+}
+
+
+
+
+
+
+
+
+
